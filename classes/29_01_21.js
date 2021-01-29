463e53bb6c98278776ad76c8225a48ac7f1da313
Angel Felipe Santaliestra Pasias <afsantaliestra@gmail.com>
2021-01-29 12:21:59 +0100

    Update todays class

--- classes/29_01_21.js
+++ classes/29_01_21.js
@@ -43,3 +43,30 @@ Mostrara al usuario una carta para que elija lo que quiere tomar.
 En esa carta, cada plato tendra un precio.
 Y habra que preguntar al usuario si quiere seguir comiendo.
 Darle platos hasta que diga que no. Al final decirle la cuenta.*/
+
+let readline = require("readline-sync");
+
+let options = "AB";
+let menu = ["A) Pizza", "B) Pasta"];
+let prices = { A: 1, B: 5 };
+let final_price = 0;
+let option, option2;
+
+do {
+  console.log("Choose you plate:");
+  console.log(menu[0]);
+  console.log(menu[1]);
+  option = readline.question("Option: ");
+
+  if (!options.includes(option)) {
+    console.log("Select between A and B.");
+    option2 = "y";
+    continue;
+  }
+
+  final_price += prices[option];
+
+  option2 = readline.question("Do yo want more? (y/n)");
+  console.log(option2);
+} while (option2 == "y");
+console.log("Fin de la cena, la cuenta es:", final_price);
